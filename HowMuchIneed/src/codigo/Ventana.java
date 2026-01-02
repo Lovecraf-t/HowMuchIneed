53c959233222b24f0f9deab84c12feb91cda3a0e
Jojo <jojosbattletendency1942@gmail.com>
2026-01-02 16:14:15 -0400

    1

--- HowMuchIneed/src/codigo/Ventana.java
+++ HowMuchIneed/src/codigo/Ventana.java
@@ -9,8 +9,9 @@ import java.awt.*;
 import java.awt.event.*;
 import java.text.*;
 import javax.swing.border.Border;
+import java.util.Scanner;
 
-public class Ventana extends JFrame {
+public class Ventana extends JFrame implements KeyListener {
     
     private JLabel evaluaciones, prioridad, popular, recomendado, valientes, migajeros, linea, notas, ponderacion, requerido, resumenText;
     private JTextField numero, prioridadCampo;
@@ -56,6 +57,8 @@ public class Ventana extends JFrame {
         prioridadCampo.setBounds(400, 48, 200, 24);
         prioridadCampo.setFont(letra1);
         prioridadCampo.setBorder(border2);
+        prioridadCampo.setText("Ninguna");
+        prioridadCampo.setEditable(false);
         add(prioridadCampo);
         
         ninguna = new JButton("Ninguna");
@@ -63,6 +66,7 @@ public class Ventana extends JFrame {
         ninguna.setFont(letra2);
         ninguna.setBackground(Color.PINK);
         ninguna.setBorder(border);
+        prioridad(ninguna, "Ninguna");
         add(ninguna);
         
         reciente = new JButton("Por fecha más reciente");
@@ -70,6 +74,7 @@ public class Ventana extends JFrame {
         reciente.setFont(letra2);
         reciente.setBackground(Color.PINK);
         reciente.setBorder(border);
+        prioridad(reciente, "Por fecha más reciente");
         add(reciente);
         
         mayor = new JButton("Por mayor ponderación");
@@ -77,6 +82,7 @@ public class Ventana extends JFrame {
         mayor.setFont(letra2);
         mayor.setBackground(Color.PINK);
         mayor.setBorder(border);
+        prioridad(mayor, "Por mayor ponderación");
         add(mayor);
         
         menor = new JButton("Por menor ponderación");
@@ -84,6 +90,7 @@ public class Ventana extends JFrame {
         menor.setFont(letra2);
         menor.setBackground(Color.PINK);
         menor.setBorder(border);
+        prioridad(menor, "Por menor ponderación");
         add(menor);
         
         popular = new JLabel("(Popular)");
@@ -142,6 +149,7 @@ public class Ventana extends JFrame {
         requeridoArea.setBounds(280, 200, 75, 320);
         requeridoArea.setFont(letra3);
         requeridoArea.setBorder(border);
+        requeridoArea.setEnabled(false);
         add(requeridoArea);
         
         calcular = new JButton("Calcular");
@@ -150,6 +158,76 @@ public class Ventana extends JFrame {
         calcular.setBackground(Color.CYAN);
         calcular.setBorder(border);
         add(calcular);
+        calcular.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                Scanner sc = new Scanner(ponderacionArea.getText());
+                Scanner sc2 = new Scanner(notasArea.getText());
+                int tam = 0;
+                int tam2 = 0;
+                while(sc.hasNextLine()){
+                    sc.nextLine();
+                    tam++;
+                }
+                sc.close();
+                while(sc2.hasNextLine()){
+                    sc2.nextLine();
+                    tam2++;
+                }
+                sc2.close();
+                if(Integer.parseInt(numero.getText()) == tam && tam == tam2){
+                    Scanner sc3 = new Scanner(notasArea.getText());
+                    Scanner sc4 = new Scanner(ponderacionArea.getText());
+                    double notasVector[] = new double[Integer.parseInt(numero.getText())];
+                    int ponderacionVector[] = new int[notasVector.length];
+                    int ubicacionProductos1[] =new int[notasVector.length];
+                    int ubicacionProductos[] =new int[notasVector.length];
+                    int iFalsa = 0;
+                    int iFalsa2 = 0;
+                    double porcentaje = 0;
+                    double acumulado = 0;
+                    double acumulador = 0;
+                    double vectorRellenar[] = new double[notasVector.length];
+                    for(int i = 0; i<notasVector.length;i++){
+                        notasVector[i] = sc3.nextDouble();
+                        if(notasVector[i] == 0.0){
+                            ubicacionProductos[iFalsa] = i;
+                            iFalsa++;
+                        }
+                        else{
+                            acumulado = 0;
+                            ubicacionProductos1[iFalsa2] = i;
+                            porcentaje = notasVector[i] * ponderacionVector[i] / 20;
+                            acumulado= porcentaje * 20 / 100;
+                            vectorRellenar[iFalsa2] = acumulado;
+                            iFalsa2++;
+                            acumulador = acumulador + acumulado;
+                        }
+                        ponderacionVector[i] = sc4.nextInt();
+                    }
+                    double vectorRellenar1[] = new double[iFalsa];
+                    double restante = 0.0; 
+                    for(int i =0; i<iFalsa;i++){
+                        restante = 0;
+                        if(acumulador <9.5){
+                            restante = 9.5 - acumulador;
+                            vectorRellenar1[i] =  restante * 20 / (ponderacionVector[ubicacionProductos[i]] * 20 / 100);
+                            acumulador =acumulador + vectorRellenar[i];
+                        }
+                    }
+                    notasArea.setText("");
+                    int buscador = 0;
+                    for(int i = 0; i<notasVector.length;i++){
+                        if(i != ubicacionProductos[buscador]){
+                            notasArea.append(""+notasVector[i]);
+                        }
+                        else{
+                            notasArea.append(""+vectorRellenar1[buscador]);
+                        }
+                        notasArea.append("\n");
+                    }
+                }
+            }
+        });
         
         borrar = new JButton("Borrar");
         borrar.setBounds(30, 590, 160, 50);
@@ -183,11 +261,37 @@ public class Ventana extends JFrame {
         add(resumenText);
         
         resumenArea = new JTextArea();
+        resumenArea.setEnabled(false);
         sp = new JScrollPane(resumenArea);
         sp.setBounds(430, 200, 330, 320);
         resumenArea.setFont(letra4);
         sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         sp.setBorder(border);
         add(sp);
+        
+        numero.addKeyListener(this);
+        ponderacionArea.addKeyListener(this);
+        notasArea.addKeyListener(this);
+    }
+    public void prioridad(JButton b, String t){
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                prioridadCampo.setText(t);
+            }
+        });
+    }
+    public void keyPressed(KeyEvent e){}
+    public void keyReleased(KeyEvent e){}
+    public void keyTyped(KeyEvent e){
+        if(e.getSource()==numero || e.getSource()==ponderacionArea){
+            if(e.getKeyChar()<'0' || e.getKeyChar()>'9'){
+                e.consume();
+            }
+        }
+        if(e.getSource()==notasArea){
+            if((e.getKeyChar()<'0' || e.getKeyChar()>'9') && e.getKeyChar() != '.'){
+                e.consume();
+            }
+        }
     }
 }
